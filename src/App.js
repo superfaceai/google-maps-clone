d1f4823c835afb6c8f40983f089494ae1071bfd3
Jan Vlnas <git.noreply@jan.vlnas.cz>
2023-05-10 11:15:33 +0200

    Marker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App() {
+  const position = [51.505, -0.09];
   return (
     <div className="App">
       <MapContainer center={[51.505, -0.09]} id="mapId" zoom={13}>
+        <Marker position={position}>
+          <Popup>Hello World</Popup>
+        </Marker>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       </MapContainer>
     </div>
