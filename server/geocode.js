15d26b480946e8d99a006e48ec221579b9937d3d
Jan Vlnas <git.noreply@jan.vlnas.cz>
2023-05-10 12:41:41 +0200

    Add marker

--- server/geocode.js
+++ server/geocode.js
@@ -1,5 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const { SuperfaceClient } = require('@superfaceai/one-sdk');
 const sdk = new SuperfaceClient();
+const PORT = 5000;
+app.use(express.json());
+app.use(cors());
+
 async function run(loc) {
   // Load the profile
   const profile = await sdk.getProfile('address/geocoding@3.1.2');
@@ -17,10 +24,20 @@ async function run(loc) {
   // Handle the result
   try {
     const data = result.unwrap();
-    console.log(data);
+    return data;
   } catch (error) {
     console.error(error);
   }
 }
 
-run('Taj Mahal');
+app.post('/cor', async (req, res) => {
+  let coordinate = {};
+  await run(req.body.loc).then((response) => {
+    coordinate.cor = response;
+    res.json(response);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App() {
   const position = [51.505, -0.09];
+
   return (
     <div className="App">
+      <div className="inputBlock">
+        <input
+          type="text"
+          id="location"
+          name="location"
+          required
+          placeholder="Enter location"
+        />
+        <div className="addloc">
+          <FontAwesomeIcon icon={faLocationDot} style={{ color: '#1EE2C7' }} />
+        </div>
+      </div>
       <MapContainer center={[51.505, -0.09]} id="mapId" zoom={13}>
         <Marker position={position}>
           <Popup>Hello World</Popup>
